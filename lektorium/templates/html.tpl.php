6fbca275009eedb6328643bff42ea836809aad6c
Anthony K <off.koss@gmail.com>
2014-04-15 16:45:38 +0400

    remove likebtn verification from template

--- lektorium/templates/html.tpl.php
+++ lektorium/templates/html.tpl.php
@@ -55,7 +55,6 @@
   <!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-<meta name="likebtn-website-verification" content="11ddd9c8ffe89840" />
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div class="skip-link">
